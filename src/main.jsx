0b40d42735d0bf44e22707cfd9e93b7be7b40ad4
NotsoJharedtroll23 <abraham.flores193@tectijuana.edu.mx>
2023-03-06 15:37:26 -0800

    REFACT: added components in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './Components/App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './Components/App';
+import Banner from './Components/Banner';
+import Body from './Components/Body';
+import Footer from './Components/Footer';
+import Header from './Components/Header'
+import './Styling/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
